1cd56e34c44610810d69f844316e096d0e767c3a
Stanislav Samoilenko <st.prog.1986@gmail.com>
2015-04-24 12:55:45 +0300

    reverting back changes to initial state

--- classes/TMM_MigrateImport.php
+++ classes/TMM_MigrateImport.php
@@ -13,77 +13,40 @@ class TMM_MigrateImport extends TMM_MigrateHelper {
 		return serialize($obj);
 	}
 
-	/* ajax */
-	public function import_content() {
-		$result = array(
-			'tables_count' => 0,
-			'attachments' => array(),
-		);
-
-		/* backup database tables */
-		if ( (bool) $_POST['backup']) {
-			$export = new TMM_MigrateExport();
-			$export->backup_data();
-		}
-
-		/* upload archive with demo data */
-		$db_upload_dir = $this->create_upload_folder();
-
-		$demofile_name = TMM_MIGRATE_PATH . 'demo_data/' . self::folder_key . '.zip';
-
-		if (file_exists($demofile_name)) {
-			copy( $demofile_name, $db_upload_dir . self::folder_key . '.zip' );
-		}
-
-		/* extract and install demo content */
+	/* calling by ajax */
+	public function import_data() {
+		$counter = 0;
 		$this->extract_zip();
-		chdir($db_upload_dir);
+		chdir($this->get_upload_dir());
 		$dat_files = glob("*.dat");
 
 		if(is_array($dat_files)){
-			$result['tables_count'] = count($dat_files);
+			$counter = count($dat_files);
 
 			foreach ($dat_files as $filename) {
 				$table = basename($filename, '.dat');
-
 				try {
-					if (@strrpos($table, '_users', -6) === false && @strrpos($table, '_usermeta', -9) === false) {
-						$this->process_table($table);
+					$this->process_table($table);
+					if(file_exists($this->get_upload_dir() . $table . '.dsc')){
+						unlink($this->get_upload_dir() . $table . '.dsc');
 					}
-					if(file_exists($db_upload_dir . $table . '.dsc')){
-						unlink($db_upload_dir . $table . '.dsc');
-					}
-					if(file_exists($db_upload_dir . $table . '.dat')){
-						unlink($db_upload_dir . $table . '.dat');
+					if(file_exists($this->get_upload_dir() . $table . '.dat')){
+						unlink($this->get_upload_dir() . $table . '.dat');
 					}
 				} catch (Exception $e) {}
 			}
-			if(file_exists($db_upload_dir . 'wpdb.prfx')){
-				unlink($db_upload_dir . 'wpdb.prfx');
+			if(file_exists($this->get_upload_dir() . 'wpdb.prfx')){
+				unlink($this->get_upload_dir() . 'wpdb.prfx');
 			}
 		}
 
-		/* upload attachments */
-		if (defined('TMM_THEME_TEXTDOMAIN')) {
-			$site_url = rtrim( get_site_url(), '/' );
-			$theme_remote_url = 'http://' . TMM_THEME_TEXTDOMAIN . '.webtemplatemasters.com';
-			$attachments = get_posts( array('post_type' => 'attachment', 'post_mime_type' => 'image, video, audio', 'numberposts' => -1) );
-
-			foreach ( $attachments as $attachment ) {
-				$result['attachments'][] = str_replace( $site_url, $theme_remote_url, $attachment->guid );
-			}
-
-		}
-
-		$this->delete_dir($db_upload_dir);
-		wp_die( json_encode($result) );
+		wp_die($counter);
 	}
 
 	public function process_table($table) {
 		global $wpdb;
-		$db_upload_dir = $this->get_upload_dir();
-		$table_dsc = unserialize(file_get_contents($db_upload_dir . $table . '.dsc'));
-		$old_wpdb_prefix = file_get_contents($db_upload_dir . 'wpdb.prfx');
+		$table_dsc = unserialize(file_get_contents($this->get_upload_dir() . $table . '.dsc'));
+		$old_wpdb_prefix = file_get_contents($this->get_upload_dir() . 'wpdb.prfx');
 		$new_table_name = preg_replace('[^' . $old_wpdb_prefix . ']', $wpdb->prefix, $table);
 
 		$wpdb->query('DROP TABLE IF EXISTS `' . $new_table_name . '`;');
@@ -153,8 +116,8 @@ class TMM_MigrateImport extends TMM_MigrateHelper {
 		$table_sql = str_replace(",);", ");", $table_sql);
 		$wpdb->query($table_sql);
 
-		/* data inserting */
-		$content = str_replace('__tmm_old_home_url__', home_url(), file_get_contents($db_upload_dir . $table . '.dat'));
+		//*** DATA INSERTING
+		$content = str_replace('__tmm_old_home_url__', home_url(), file_get_contents($this->get_upload_dir() . $table . '.dat'));
 		$content = str_replace('__tmm_wpdb_prefix__', $wpdb->prefix, $content);
 		$content = str_replace('stdClass::__set_state', 'self::set_state', $content);
 
@@ -178,66 +141,5 @@ class TMM_MigrateImport extends TMM_MigrateHelper {
 			}
 		}
 	}
-
-	function upload_attachment( $url ) {
-		if (!empty($_POST['url'])) {
-			$url = $_POST['url'];
-		}
-
-		$msg = '';
-		$errormsg = '';
-
-		/* create placeholder file in the upload dir */
-		$file_name = basename( $url );
-		$tmp_pos = strpos($url, '/uploads/');
-
-		if ($tmp_pos === false) {
-			$errormsg = 'wrong file path';
-		}
-
-		$tmp_pos += 9;
-		$post_date_format = substr($url, $tmp_pos, 7);
-		$upload_dir = $this->get_wp_upload_dir();
-
-		if ( file_exists( $upload_dir. substr($url, $tmp_pos) ) ) {
-			$errormsg = 'file already exists';
-		}
-
-		$upload = wp_upload_bits( $file_name, 0, '', $post_date_format );
-
-		if ( $upload['error'] ) {
-			$errormsg = $upload['error'];
-		}
-
-		if ( !wp_check_filetype( $upload['file'] ) ) {
-			$errormsg = 'invalid file type';
-		}
-
-		/* fetch the remote url and write it to the placeholder file */
-		$headers = wp_get_http( $url, $upload['file'] );
-
-		if ( !$headers || $headers['response'] != '200' ) {
-			@unlink( $upload['file'] );
-			$errormsg = 'remote server did not respond';
-		}
-
-		$filesize = @filesize( $upload['file'] );
-
-		if ( !$filesize || (isset($headers['content-length']) && $filesize != $headers['content-length']) ) {
-			//@unlink( $upload['file'] );
-			$errormsg = 'incorrect file size';
-		}
-
-		if ($errormsg) {
-			$msg = 'Failure: ' . $errormsg . ' - ' . $url;
-		}
-
-		if (!empty($_POST['url'])) {
-			echo ($msg);exit();
-		} else {
-			return $msg;
-		}
-
-	}
-
+	
 }
