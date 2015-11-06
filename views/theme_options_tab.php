30791d0655af6a32bc9653e96edcb370ce532a3d
Oleksiy Ostapenko <lelik4w@gmail.com>
2015-11-06 18:37:33 +0100

    edit description field's text

--- views/theme_options_tab.php
+++ views/theme_options_tab.php
@@ -287,7 +287,7 @@ TMM_OptionsHelper::draw_theme_option(array(
 	'name' => 'locations_zip',
 	'type' => 'upload_zip',
 	'default_value' => '',
-	'description' => __('Only zip files.<br> While importing big countries you need to login to your Cpanel and increase the server waiting time to 300 sec (max_execution_time in PHP settings) and increase the  memory_limit to  128M as well.', TMM_MIGRATE_TEXTDOMAIN),
+	'description' => __('Only zip files.<br> While importing big countries you need to login to increase the server execution time to 300 sec (max_execution_time in PHP settings) and increase the  memory_limit to  128M as well.', TMM_MIGRATE_TEXTDOMAIN),
 	'id' => 'upload_locations',
 ));
 ?>
