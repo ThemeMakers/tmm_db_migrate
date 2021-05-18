71c0c5157095022df81a164ad859db7587899f80
Oleksiy Ostapenko <lelik4w@gmail.com>
2021-05-18 23:57:04 +0200

    update plugin version

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://webtemplatemasters.com
  * Description: ThemeMakers WordPress DataBase Migration
  * Author: ThemeMakers
- * Version: 2.0.9
+ * Version: 2.1.0
  * Author URI: http://themeforest.net/user/ThemeMakers
  * Text Domain: tmm_db_migrate
  */
